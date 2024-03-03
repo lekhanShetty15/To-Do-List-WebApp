7580bcdd19f498888a73bd476908d3f847492eec
lekhanShetty15 <lekhanshetty2000@gmail.com>
2024-03-03 23:06:21 +0530

    initial file added

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+function addTask() {
+    const input = document.getElementById("input").value;
+    const description = document.getElementById("description").value;
+
+    if (input && description) {
+        let taskOutput = document.getElementById("display");
+        taskOutput.innerHTML += `
+        <div class="task">
+        <div class="titleDiv">${input}</div>
+        <div class="descDiv">${description}</div>
+        <button class="deleteDiv" onclick="removeTask(this.parentNode)"><img class="delete" src="./icon/pngwing.com.png" alt=""></button>
+    </div>
+        `;
+
+        document.getElementById("input").value = "";
+        document.getElementById("description").value = "";
+    } else {
+        alert("Please enter all the fields");
+    }
+}
+
+function removeTask(del){
+    del.remove()
+}
+
+
+
+
+
+
+
+
+
+
+
+
